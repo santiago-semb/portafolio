21bfe474cab6fe8cc942aa15406d01cd868afc65
sancho <sansembbb@gmail.com>
2022-12-05 11:22:46 -0300

    portafolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './error404/error404.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'home',pathMatch:'full'},
+  {path:'home',component: HomeComponent},
+  {path:'exp',component: ExperienciaComponent},
+  {path:'**', component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { Error404Component } from './error404/error404.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavComponent,
+    ExperienciaComponent,
+    Error404Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
